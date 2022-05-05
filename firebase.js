1817da73442e145b6738c17f6cffdc878549a271
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-05 21:15:08 +0100

    google auth2 working

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWlEOjz8sMgZbnemmA5_yoR-iMFtmraF8",
+  authDomain: "clone-d5a3d.firebaseapp.com",
+  projectId: "clone-d5a3d",
+  storageBucket: "clone-d5a3d.appspot.com",
+  messagingSenderId: "626151546442",
+  appId: "1:626151546442:web:2ba128c408d03445d590b3",
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Header() {
   return (
@@ -30,7 +31,7 @@ function Header() {
         </div>
         {/* Right */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
+          <div onClick={signIn} className="link">
             <p>Hello Banksy</p>
             <p className="font-extrabold md:text-sm">Account & Lists </p>
           </div>
