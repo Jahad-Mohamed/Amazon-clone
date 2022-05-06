75668541f47344576c6396e811f98740c2c6ed4d
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-06 18:00:20 +0100

    added redirect

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,19 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function Header() {
+  const [session] = useSession();
+  const [router] = useRouter();
+
   return (
     <header>
       {/* Top Nav  */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
