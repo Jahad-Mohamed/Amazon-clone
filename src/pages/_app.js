2b1393191c48891f31219d02833c4d4bb6897f80
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-07 19:55:13 +0100

    error fixed! changed auth provider to sessionprovider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,16 @@
 import { Provider } from "react-redux";
 import { store } from "../app/store";
 import "../styles/globals.css";
-import { Provider as AuthProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 
-const MyApp = ({ Component, pageProps }) => {
+function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session}>
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
-    </AuthProvider>
+    </SessionProvider>
   );
-};
+}
 
 export default MyApp;
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,8 +3,10 @@ import Header from "../components/Header";
 
 function Checkout() {
   return (
-    <div className="">
+    <div className="bg-gry-100">
       <Header />
+
+      <main className=" lg:flex max-w-screen-2xl mx-auto"></main>
     </div>
   );
 }
